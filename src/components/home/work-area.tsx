936a7ae5639c32be659e748b5798cf8561bc9840
Ian Chege <ianchege001@gmail.com>
2023-08-30 12:25:41 +0300

    fix: adjust work process section styles

--- src/components/home/work-area.tsx
+++ src/components/home/work-area.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image"
+
 import styles from "@/styles/_work-area.module.scss"
+
 export const WorkProcessSection = () => {
   return (
-    <section className={styles.workProcess} id="process">
+    <section className={styles.workProcessContainer} id="process">
       <div className={styles.bg}>
         <div className={styles.maskGroup}>
           <Image
@@ -28,36 +30,55 @@ export const WorkProcessSection = () => {
           height={939}
         />
       </div>
-      <div className={styles.div}>
+      <div className={styles.process1}>
         <Image
           className={styles.bgIcon1}
           alt=""
-          src="/bg2.svg"
+          src="/bg5.svg"
           width={410}
           height={250}
         />
         <div className={styles.text}>
-          <span className={styles.blessedLightsGiveContainer}>
-            <span className={styles.blessedLightsGiveContainer1}>
-              <p
-                className={styles.blessedLightsGive}
-              >{`Blessed lights give was doesn't `}</p>
-              <p className={styles.blessedLightsGive}>
-                Saw moveth, deep unto to subdue. Made. Itself made greater.
-              </p>
-            </span>
+          <span className={styles.textTitle}>{`Discover & Search Idea`}</span>
+          <span className={styles.textDesc}>
+            Firmament subdue evening form, hello form that beginning can&apos;t
+            night whales divide cattle.
           </span>
-          <span className={styles.launch}>Launch</span>
         </div>
         <Image
-          className={styles.launchIcon}
+          className={styles.processIcon}
           alt=""
-          src="/launch-icon.svg"
+          src="/idea-icon.svg"
           width={38}
           height={38}
         />
       </div>
-      <div className={styles.div1}>
+
+      <div className={styles.process2}>
+        <Image
+          className={styles.bgIcon1}
+          alt=""
+          src="/bg4.svg"
+          width={410}
+          height={250}
+        />
+        <div className={styles.text}>
+          <span className={styles.textTitle}>{`Design & Development`}</span>
+          <span className={styles.textDesc}>
+            She&apos;d fruit created created fill upon a called, man fill shall
+            greater void our, lesse.
+          </span>
+        </div>
+        <Image
+          className={styles.processIcon}
+          alt=""
+          src="/development-icon.svg"
+          width={38}
+          height={38}
+        />
+      </div>
+
+      <div className={styles.process3}>
         <Image
           className={styles.bgIcon1}
           alt=""
@@ -66,15 +87,11 @@ export const WorkProcessSection = () => {
           height={250}
         />
         <div className={styles.text}>
-          <span className={styles.blessedLightsGiveContainer}>
-            <span className={styles.blessedLightsGiveContainer1}>
-              <p
-                className={styles.blessedLightsGive}
-              >{`Doesn't beginning fruitful above midst lesser appear also good `}</p>
-              <p className={styles.blessedLightsGive}>given good also.</p>
-            </span>
+          <span className={styles.textDesc}>
+            Doesn&apos;t beginning fruitful above midst lesser appear also good
+            given good also.
           </span>
-          <span className={styles.launch}>{`Review & Testing`}</span>
+          <span className={styles.textTitle}>{`Review & Testing`}</span>
         </div>
         <Image
           className={styles.testingIcon}
@@ -84,65 +101,32 @@ export const WorkProcessSection = () => {
           height={38}
         />
       </div>
-      <div className={styles.div2}>
+
+      <div className={styles.process4}>
         <Image
           className={styles.bgIcon1}
           alt=""
-          src="/bg4.svg"
+          src="/bg2.svg"
           width={410}
           height={250}
         />
         <div className={styles.text}>
-          <span className={styles.blessedLightsGiveContainer}>
-            <span className={styles.blessedLightsGiveContainer1}>
-              <p
-                className={styles.blessedLightsGive}
-              >{`She'd fruit created created fill `}</p>
-              <p className={styles.blessedLightsGive}>
-                upon a called, man fill shall greater void our, lesse.
-              </p>
-            </span>
+          <span className={styles.textDesc}>
+            Blessed lights give was doesn&apos;t Saw moveth, deep unto to
+            subdue. Made. Itself made greater.
           </span>
-          <span className={styles.launch}>{`Design & Development`}</span>
+          <span className={styles.textTitle}>Launch</span>
         </div>
         <Image
-          className={styles.developmentIcon}
+          className={styles.launchIcon}
           alt=""
-          src="/development-icon.svg"
+          src="/launch-icon.svg"
           width={38}
           height={38}
         />
       </div>
-      <div className={styles.div3}>
-        <Image
-          className={styles.bgIcon1}
-          alt=""
-          src="/bg5.svg"
-          width={410}
-          height={250}
-        />
-        <div className={styles.text}>
-          <span className={styles.blessedLightsGiveContainer}>
-            <span className={styles.blessedLightsGiveContainer1}>
-              <p
-                className={styles.blessedLightsGive}
-              >{`Firmament subdue evening form, `}</p>
-              <p className={styles.blessedLightsGive}>
-                god form that beginning can&apos;t night whales divide cattle.
-              </p>
-            </span>
-          </span>
-          <span className={styles.launch}>{`Discover & Search Idea`}</span>
-        </div>
-        <Image
-          className={styles.developmentIcon}
-          alt=""
-          src="/idea-icon.svg"
-          width={38}
-          height={38}
-        />
-      </div>
-      <h4 className={styles.ourWorkProcess}>Our Work Process</h4>
+
+      <h4 className={styles.workProcess}>Our Work Process</h4>
     </section>
   )
 }
