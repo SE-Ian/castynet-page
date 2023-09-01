d36baad2329a1a084c19fac359a692f4533c59b2
Ian Chege <ianchege001@gmail.com>
2023-09-01 09:45:57 +0300

    fix: remove ignoreBuildErrors from next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const path = require('path')
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
     }
 }
 
